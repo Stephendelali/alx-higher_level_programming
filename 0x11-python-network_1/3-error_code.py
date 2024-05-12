18ab0ff5dbd65729c343007577bc7919a5ebaf69
Stephen <amankwastephen06@gmail.com>
2024-05-12 11:44:52 -0700

    file

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """ Python script that takes in a URL, sends a request to the URL and displays
 the value of the X-Request-Id variable found in the header of the response.."""
-
 import sys
 import urllib.request
 import urllib.parse
 
-if __name__ == '_main_':
+
+if __name__ == "_main_":
     url = sys.argv[1]
+    
     request = urllib.request.Request(url)
     try :
         with urllib.request.urlopen(request) as response:
             print(response.read().decode("ascii"))
     except urlib.error.HTTPError as e:
-        print ("Error code: ".format (e.code))  
+        print ("Error code:{} ".format (e.code))  
